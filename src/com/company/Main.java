94df84c0c09495392c2dba4cbc5da7a4bbfaa475
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-11-13 15:29:43 +0800

    M2Q.22 直角三角形邊長

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,24 @@
 package com.company;
 
 import java.util.Scanner;
-import  java.math.BigInteger;
+
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         int x=sc.nextInt();
-  BigInteger a=new BigInteger("1");
-  int b=0;
-  for (int c=x;c>1;c--){
-      a=a.multiply(new BigInteger(Integer.toString(c)));
-  }
-  while (a.mod(new BigInteger("10")).intValue()==0){
-      b++;
-      a=a.divide(new BigInteger("10"));
+        for (int a=3;a<=x;a++){
+            for (int b=a+1;b<=x;b++){
+                for (int c=b+1;c<x;c++){
+                    if ((a+b+c)<=x){
+                        if ((a*a+b*b)==c*c){
+                            System.out.println(a+"\t"+b+"\t"+c);
+                        }
+                    }
+                }
+            }
         }
-        System.out.println(b);
+
     }
 }
 
