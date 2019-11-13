025f8a5df59a16e2be866af56ec75f79c62ccc24
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-11-13 15:40:42 +0800

    M2Q.23 數字反轉

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,19 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
-        int x=sc.nextInt();
-        for (int a=3;a<=x;a++){
-            for (int b=a+1;b<=x;b++){
-                for (int c=b+1;c<x;c++){
-                    if ((a+b+c)<=x){
-                        if ((a*a+b*b)==c*c){
-                            System.out.println(a+"\t"+b+"\t"+c);
-                        }
-                    }
-                }
-            }
-        }
-
+       long x=sc.nextLong();
+       while (x>0){
+           System.out.print(x%10);
+           x/=10;
+       }
+        System.out.println();
     }
 }
 
