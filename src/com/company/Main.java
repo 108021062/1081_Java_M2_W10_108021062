8e66ce8357daef02a69954edd62ebca181871733
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-11-13 15:11:38 +0800

    M2Q.21.20 算幾個整數的0

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,22 @@
 package com.company;
 
 import java.util.Scanner;
-
+import  java.math.BigInteger;
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         int x=sc.nextInt();
-     int count =0,res=1;
-     for (int a=x;a>1;a--){
-         res*=a;
-     }
-     while (res %10==0){
-         count++;
-         res/=10;
-     }
-        System.out.println(count);
+  BigInteger a=new BigInteger("1");
+  int b=0;
+  for (int c=x;c>1;c--){
+      a=a.multiply(new BigInteger(Integer.toString(c)));
+  }
+  while (a.mod(new BigInteger("10")).intValue()==0){
+      b++;
+      a=a.divide(new BigInteger("10"));
+        }
+        System.out.println(b);
     }
 }
 
